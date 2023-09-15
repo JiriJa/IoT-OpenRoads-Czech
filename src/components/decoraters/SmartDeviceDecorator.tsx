2ebd91393e5a121c6051155eff7fdb39c5c93b01
JiriJa <jiri.jarolim@volny.cz>
2023-09-15 15:11:58 +0200

    Senzometrics in Rural use

--- src/components/decoraters/SmartDeviceDecorator.tsx
+++ src/components/decoraters/SmartDeviceDecorator.tsx
@@ -45,7 +45,7 @@ export class SmartDeviceDecorator implements Decorator {
         values.forEach(value => {
             const smartDeviceMarker = new SmartDeviceMarker(
                 { x: value.origin.x, y: value.origin.y, z: value.origin.z },
-                { x: 55, y: 55 },
+                { x: 25, y: 25 },
                 value.smartDeviceId,
                 value.smartDeviceType,
                 cloudData[value.smartDeviceId],
