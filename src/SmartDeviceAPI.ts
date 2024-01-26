2bd213de998b2928327e43374bebafb7b462972d
JiriJa <jiri.jarolim@volny.cz>
2024-01-26 11:37:09 +0100

    warmup JSON souborsiiselitwin3x.json

--- src/SmartDeviceAPI.ts
+++ src/SmartDeviceAPI.ts
@@ -3,7 +3,8 @@ export class SmartDeviceAPI {
     public static async getData() {
         
         // const response = await fetch("https://smarthomedata.z22.web.core.windows.net/");
-        const response = await fetch("https://raw.githubusercontent.com/JiriJa/IoT_Repository/main/soubor.json");
+        // const response = await fetch("https://raw.githubusercontent.com/JiriJa/IoT_Repository/main/soubor.json");
+        const response = await fetch("https://raw.githubusercontent.com/JiriJa/IoT_Repository/main/souborsiiselitwin3x.json");
         const deviceData = response.json();
 
         return deviceData;
